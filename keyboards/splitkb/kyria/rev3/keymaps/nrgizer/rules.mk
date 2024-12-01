58fda20536bd843477d9b19906c9e1cb0d00b96a
Carl-Magnus Björkell <calle@gibberish.dk>
2024-12-01 08:10:35 +0100

    Port old keymap to userspace setup
    Pretty much pull my old keymap for rev1 line for line to the new
    userspace rev3 setup, while also trying to make sure that it fits with
    any new stuff.
    
    Should probably try out both RGB_MATRIX and DYNAMIC_MACROS if/when this
    works.

---
+++ keyboards/splitkb/kyria/rev3/keymaps/nrgizer/rules.mk
@@ -0,0 +1,10 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = no  # This doesn't work. The OLEDs die, and the whole kb becomes weird. (try again now on rev3?)
+
+CONVERT_TO=liatris
